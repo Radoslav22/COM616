3f54d8412f390a73b91252a6dd794410f96ee287
Radoslav22 <4takvr26@solent.ac.uk>
2023-02-28 23:56:35 +0000

    Added navbar with two menus; first try to display the list of restaurants on the left side of the screen

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,13 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
+import NavBar from "../Components/navbar";
 import GoogleMap from "../Components/googlemap";
-class Home extends Component {
-    render() {
-        return (
-            <div style={{ width: "100%", height: "100%" }}>
-                <GoogleMap></GoogleMap>
-            </div>
-        );
-    }
+import { Box } from '@mui/system';
+import { Typography, Grid } from '@mui/material';
+
+function Home() {
+
+    const message = "Hello Restaurants"
+    return (
+        <div>
+            <NavBar></NavBar>
+            <GoogleMap></GoogleMap>
+            <Box sx={{ position: "absolute", top: "9%", left: 0, background: "yellow", marginRight: "80%", padding: "2px" }}>
+                <Grid sx={{ paddingBottom: "2px" }}>
+                    <Typography noWrap>{message}</Typography>
+                </Grid>
+                <Grid sx={{ paddingBottom: "2px" }}>
+                    <Typography noWrap>{message}</Typography>
+                </Grid>
+                <Grid sx={{ paddingBottom: "2px" }}>
+                    <Typography noWrap>{message}</Typography>
+                </Grid>
+                <Grid sx={{ paddingBottom: "2px" }}>
+                    <Typography noWrap>{message}</Typography>
+                </Grid>
+                <Grid sx={{ paddingBottom: "2px" }}>
+                    <Typography noWrap>{message}</Typography>
+                </Grid>
+            </Box>
+
+
+
+        </div>
+    );
+
 }
 
 export default Home;
