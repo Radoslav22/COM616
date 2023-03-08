ca40396fd1a50f181e4dc83abf61f0312ac325d8
Radoslav22 <4takvr26@solent.ac.uk>
2023-03-08 14:47:53 +0000

    Added config folder with firebase configuration

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA4y71feTE9m_zzox8GxuMh_pvXcvw3Vy0",
+    authDomain: "com616-40d65.firebaseapp.com",
+    projectId: "com616-40d65",
+    storageBucket: "com616-40d65.appspot.com",
+    messagingSenderId: "207965384291",
+    appId: "1:207965384291:web:4d3d37803fb78950341256",
+    measurementId: "G-9RZZ6BR4YM"
+};
+
+export default firebaseConfig;
